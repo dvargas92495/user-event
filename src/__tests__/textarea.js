86090f2e1f3447d60b9f80e216906df76e856bb4
David Vargas <dvargas92495@gmail.com>
2020-09-11 15:31:48 -0400

    Using opts instead

--- src/__tests__/textarea.js
+++ src/__tests__/textarea.js
@@ -4,7 +4,6 @@ import {setup} from './helpers/utils'
 test('textarea selection influences typing', async () => {
     const {element} = setup('<textarea />');
     await userEvent.type(element, "Example Text");
-    element.setSelectionRange(0,0);
-    await userEvent.type(element, "Prefix: ");
+    await userEvent.type(element, "Prefix: ", {initialSelectionStart: 0, initialSelectionEnd: 0});
     expect(element.value).toBe("Prefix: Example Text");
 });
